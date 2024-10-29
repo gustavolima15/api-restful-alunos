da203e1ebd083970dc5226bcf47799158df8b8e8
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-10-28 22:07:16 -0300

    feat: rota Post

---
+++ repositories/alunoRepository.js
@@ -0,0 +1,14 @@
+const { v4: uuidv4 } = require('uuid');
+
+let alunos = [];
+
+function create({ nome, email, nome_curso }) {
+  const aluno = {
+    id: uuidv4(),
+    nome,
+    email,
+    nome_curso,
+  };
+  alunos.push(aluno);
+  return aluno;
+}
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World'); 
-});
+app.post('/alunos', (req, res) => {
+    const { nome, email, nome_curso } = req.body;
+    const aluno = create({ nome, email, nome_curso });
+    res.status(201).json(aluno);
+  });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
